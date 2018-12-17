32bcfc726532b1663a2ae8ab5396d39fe183fde3
maheir <maheirne@gmail.com>
2018-12-17 14:41:11 +0100

    VARIABLE

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            Console.WriteLine("voici les nombres" +"x" + x + "y" + y + "z" + z);
+            int sum = (x + y);
+            int product = (sum) * z;
+            Console.WriteLine("le resultat est" +sum*z);
+
+
+        }
+    }
+}
